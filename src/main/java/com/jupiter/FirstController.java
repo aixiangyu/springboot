d2972c423395faa3aae474189eb88b141e94064f
aixiangyu <jupiter.aixiangyu@qq.com>
2017-03-28 08:56:05 +0800

    springboot init

---
+++ src/main/java/com/jupiter/FirstController.java
@@ -0,0 +1,25 @@
+package com.jupiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by aixiangyu on 2017/3/2.
+ */
+@RestController
+@EnableAutoConfiguration
+public class FirstController {
+
+    @RequestMapping("/")
+    public String home(){
+
+        return "hello world!";
+    }
+
+    public static void main(String[] args) throws  Exception {
+        SpringApplication.run(FirstController.class,args);
+
+    }
+}
